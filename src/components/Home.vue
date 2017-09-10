c34bff7e27adc3abcc42a680fdb29e296ed44299
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2017-09-10 17:56:56 +0300

    Homepage is now showing recent chats.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,19 @@
     <v-container>
       <v-layout row wrap>
         <v-flex xs12 class="text-xs-center">
-          <h3>Welcome to the most unfunctional page of this web app!</h3>
-          <v-btn large to="/chats" class="info">Look for chats</v-btn>
+          <h3>Select a chat to start! Or create yours. </h3>
+          <chats></chats>
         </v-flex>
       </v-layout>
     </v-container>
   </div>
 </template>
+
+<script>
+  import Chats from './Chat/Chats.vue'
+  export default {
+    components: {
+      'chats': Chats
+    }
+  }
+</script>
